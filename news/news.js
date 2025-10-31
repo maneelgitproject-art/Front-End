f331e7afc5ae256aa973ba14e72bc379058cd522
maneelgitproject-art <maneelgitproject@gmail.com>
2025-10-31 07:23:26 +0100

    changed html,css,menu  img files

--- news/news.js
+++ news/news.js
@@ -1,1 +1,66 @@
 // News js Code
+ 
+const overview_link = document.getElementById("overview-link");
+const overview_link_underline = document.querySelector(
+    ".overview-link-item-underline"
+);
+const news_link = document.getElementById("news-link");
+const news_link_underline = document.querySelector(".news-link-item-underline");
+const specialties_link = document.getElementById("specialties-link");
+const specialties_link_underline = document.querySelector(
+    ".specialties-link-item-underline"
+);
+const find_specialize_link = document.getElementById("find_specialize-link");
+const find_specialize_link_underline = document.querySelector(
+    ".find_specialize-link-item-underline"
+);
+const support_link = document.getElementById("support-link");
+const support_link_underline = document.querySelector(
+    ".support-link-item-underline"
+);
+const social_link = document.getElementById("social-link");
+const social_link_underline = document.querySelector(
+    ".social-link-item-underline"
+);
+
+// Change Header Links Style
+change_link_style(overview_link, overview_link_underline);
+change_link_style(news_link, news_link_underline);
+change_link_style(specialties_link, specialties_link_underline);
+change_link_style(find_specialize_link, find_specialize_link_underline);
+change_link_style(support_link, support_link_underline);
+change_link_style(social_link, social_link_underline);
+
+// Reset Header Links Style
+reset_link_style(overview_link, overview_link_underline);
+reset_link_style(news_link, news_link_underline);
+reset_link_style(specialties_link, specialties_link_underline);
+reset_link_style(find_specialize_link, find_specialize_link_underline);
+reset_link_style(support_link, support_link_underline);
+reset_link_style(social_link, social_link_underline);
+
+// Change Header Links Style Function
+function change_link_style(element, elementUnderLine) {
+    element.addEventListener("mouseover", function () {
+        element.style.backgroundColor = "rgba(160, 159, 159, 0.55)";
+        element.style.borderRadius = "4px";
+        element.style.cursor = "pointer";
+        element.style.color = "white";
+        elementUnderLine.style.backgroundColor = "#32c7ff";
+        elementUnderLine.style.position = "absolute";
+        elementUnderLine.style.bottom = "-20px";
+        elementUnderLine.style.left = "0";
+        elementUnderLine.style.height = "5px";
+        elementUnderLine.style.width = "100%";
+        elementUnderLine.style.borderRadius = "2px";
+        elementUnderLine.style.transition = "0.1s ease-in-out";
+    });
+}
+
+// Reset Header Links Style Function
+function reset_link_style(element, elementUnderLine) {
+    element.addEventListener("mouseout", function () {
+        element.style.backgroundColor = "";
+        elementUnderLine.style = "";
+    });
+}
