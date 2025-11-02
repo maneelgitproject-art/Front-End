fe9877bdc8e0e593428f9df2c96b8b6e3065b1ce
maneelgitproject-art <maneelgitproject@gmail.com>
2025-11-02 17:04:44 +0100

    update css js and html for join

--- join_project/join.js
+++ join_project/join.js
@@ -1,1 +1,40 @@
-// join js code
+const progressBar = document.getElementById('progressBar');
+const progressBar2 = document.getElementById('progressBar2');
+
+const nextBtn = document.getElementById('nextBtn');
+const prevBtn = document.getElementById('prevBtn');
+const pages = document.querySelectorAll('.page');
+
+let currentPage = 0;
+
+nextBtn.addEventListener('click', () => {
+  if (currentPage < pages.length - 1) {
+    pages[currentPage].classList.remove('active');
+    currentPage++;
+    pages[currentPage].classList.add('active');
+    moveProgress();
+  }
+});
+
+prevBtn.addEventListener('click', () => {
+  if (currentPage > 0) {
+    pages[currentPage].classList.remove('active');
+    currentPage--;
+    pages[currentPage].classList.add('active');
+    moveProgress();
+  }
+});
+
+function moveProgress() {
+  const progressWidth = document.querySelector('.progress').offsetWidth;
+  const barWidth = progressBar.offsetWidth;
+  const moveAmount = progressWidth - barWidth;
+
+   progressBar.style.transform = `translateX(-${moveAmount * currentPage}px)`;
+   progressBar2.style.transform = `translateX(-${moveAmount * currentPage}px)`;
+
+}
+
+ 
+    
+    
