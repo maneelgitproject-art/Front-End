5b65147aed0570330d3ea5ba374ffbbcf425d6d2
maneelgitproject-art <maneelgitproject@gmail.com>
2025-11-02 23:46:39 +0100

    menu up css icon ..

--- news/news.js
+++ news/news.js
@@ -1,1 +1,15 @@
 // News js Code
+
+ document.addEventListener('DOMContentLoaded', () => {
+    const toggle = document.getElementById('menu-toggle');
+    const closeBtn = document.getElementById('menu-close');
+    const links = document.getElementById('links');
+
+    toggle.addEventListener('click', () => {
+        links.classList.add('show'); // show menu
+     });
+
+    closeBtn.addEventListener('click', () => {
+        links.classList.remove('show'); // hide menu
+    });
+});
