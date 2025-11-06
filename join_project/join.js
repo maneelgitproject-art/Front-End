72ef05f4d8ce6ef49994d4836b53b61c9db7c97b
maneelgitproject-art <maneelgitproject@gmail.com>
2025-11-06 19:24:37 +0100

    bg responsive menu page join

--- join_project/join.js
+++ join_project/join.js
@@ -98,4 +98,20 @@ function moveProgress() {
     document.querySelectorAll('.dropdown').forEach(d => d.classList.remove('open'));
     document.querySelectorAll('.select-btn').forEach(b => b.classList.remove('open'));
   });
+
+  
+
+   document.addEventListener('DOMContentLoaded', () => {
+    const toggle = document.getElementById('menu-toggle');
+    const closeBtn = document.getElementById('menu-close');
+    const links = document.getElementById('links');
+
+    toggle.addEventListener('click', () => {
+        links.classList.add('show'); // show menu
+     });
+
+    closeBtn.addEventListener('click', () => {
+        links.classList.remove('show'); // hide menu
+    });
+});
  
