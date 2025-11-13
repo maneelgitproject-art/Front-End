a21e21886c52411ede64d4f9e646590d2d998b6b
AbdoEssam2024 <eabdo2051@gmail.com>
2025-11-13 20:06:38 +0200

    Update navbar:  fixes it in all pages, added responsive files headercss in CSS helpers and media css (#13)
    Co-authored-by: maneelgitproject-art <maneelgitproject@gmail.com>

--- index/index.js
+++ index/index.js
@@ -25,3 +25,10 @@ reset_link_style(specialties_link, specialties_link_underline);
 reset_link_style(find_specialize_link, find_specialize_link_underline);
 reset_link_style(support_link, support_link_underline);
 reset_link_style(social_link, social_link_underline);
+
+
+
+
+
+
+
